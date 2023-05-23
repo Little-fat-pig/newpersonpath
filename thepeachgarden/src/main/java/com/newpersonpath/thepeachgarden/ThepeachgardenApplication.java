ed058f185ef466fce7c9fc869591ff6ca8ed744a
zhangxueliang7 <zxlmufu@163.com>
2023-05-23 13:54:17 +0800

    【add】本次预备提交excel工具的使用和字符串算法的提交

---
+++ thepeachgarden/src/main/java/com/newpersonpath/thepeachgarden/ThepeachgardenApplication.java
@@ -0,0 +1,13 @@
+package com.newpersonpath.thepeachgarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThepeachgardenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThepeachgardenApplication.class, args);
+    }
+
+}
